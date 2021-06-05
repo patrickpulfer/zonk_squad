5202a76b0b910b1ab68d1a91bfae3cae78b6e774
patrickpulfer <patrick.pulfer1@gmail.com>
2021-06-05 01:24:31 +0100

    Feature: Chnage from HTML to PHP application

---
+++ templates/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Zonk Squad.Club</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#2E2E2E">
+    <link id="favicon" rel="shortcut icon" href="/static/img/favicon.ico">
+    <link rel="stylesheet" href="/static/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/static/css/mdb.min.css">
+    <link rel="stylesheet" href="/static/css/app.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark elegant-color-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="https://images.evetech.net/corporations/98234482/logo?size=128" height="50"
+                    alt="Zonk Squad">Zonk Squad
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+                aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="basicExampleNav">
+                <ul class="navbar-nav mr-auto">
+                </ul>
+                <span class="navbar-text white-text">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a class="nav-link" href="https://discord.zonksquad.club"
+                                target="_blank">Discord</a></li>
+                        <li class="nav-item"><a class="nav-link" href="ts3server://ts.zonksquad.club">TeamSpeak</a></li>
+                        <li class="nav-item"><a class="nav-link"
+                                href="mumble://mumble.goonfleet.com/Corporation Channels/Zonk Squad?title=Goonfleet&version=1.2.0">Mumble</a>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="https://seat.zonksquad.club"
+                                target="_blank">SeAT</a></li>
+                    </ul>
+                </span>
+            </div>
+        </div>
+    </nav>
---
+++ templates/initial.php
@@ -0,0 +1,56 @@
+<main>
+    <div class="view jarallax" style="height: 100vh;">
+        <img class="jarallax-img" src="/static/img/backgrounds/Screenshot_20200710_203315.png" alt="">
+        <div class="mask rgba-blue-slight">
+            <div class="container flex-center text-center">
+                <div class="row mt-5">
+                    <div class="col-md-12 wow fadeIn mb-3 text-white">
+                        <h1 class="display-3 mb-2 wow fadeInDown" data-wow-delay="0.3s">ZONK SQUAD</h1>
+                        <h5 class="text-uppercase mb-3 mt-1 font-weight-bold wow fadeIn" data-wow-delay="0.4s">An Eve Online Corporation</h5>
+                        <a class="btn btn-dark wow fadeIn" data-wow-delay="0.4s" href="#about">About</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <article class="container" id="about">
+        <div class="row mb-5 pb-4">
+            <div class="col-md-12 text-center">
+                <h2 class="h1 font-weight-bold light-blue-text my-5 py-4">Zonk Squad, proud member of The Imperium</h2>
+                <br>
+                <p>
+                    Zonk Squad is a multinational corporation based in Null Security Space. This means we have a german, english and portuguese speaking wing. We are proud members of the Imperium, largest coalition in New Even</p>
+                <p>
+                <h3>What we offer</h3>
+                <ul class="text-left">
+                    <li>Solo, small gang or large supecapital warfare</li>
+                    <li>Mining, Industry, Exploration</li>
+                    <li>A place to make friends and call Home</li>
+                </ul>
+
+                    <h3>What we expect from our members?</h3>
+                    <ul class="text-left">
+                        <li>Fleet participation for strategic objectives</li>
+                        <li>Presence in Jabber, Forum, Discord and Voice Chat</li>
+                    </ul>
+
+                    <h3>What you can expect from ZonkSquad?</h3>
+                    <ul class="text-left">
+                        <li>Friendly environment where newer players are welcome, training and tons of information
+                            available to improve your ingame skills</li>
+                        <li>Healthy relationship between all corp members with many fun moments</li>
+                        <li>Guidance from senior members to quickly reach your ingame goals</li>
+                        <li>Alliance forum with many information available</li>
+                        <li>Easy ways to make isk and fuel your ambitions</li>
+                    </ul>
+
+                    </p>
+                </div>
+            </div>
+        </article>
+    
+    <div class="view jarallax">
+        <img class="jarallax-img" src="/static/img/backgrounds/Screenshot_20200704_235313.png" lazy alt="">
+    </div>
+</main>
